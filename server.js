e0c0a85981dad88cb5e1f7f55c8f2ccf2a14eee5
lucas <lucasdjgalberto@gmail.com>
2025-08-27 12:57:34 -0300

    Funcionamento de envio e recebimento de resposta concluído no momento.

--- server.js
+++ server.js
@@ -1,18 +1,25 @@
 // primeiro passo e importar o express
 import express from 'express';
+
 // é necessário criar uma variável para utilizar o express
 const app = express();
 
+// importante informar que utilizaremos o json para troca de informações
+app.use(express.json());
+
+// rota para envio de informações do usuário
 const users = [];
-
 app.post('/users',(req, res) =>{
-    console.log(req)
 
-    res.send('Ok, aqui deu certo')
+    users.push(req.body);
+
+    res.status(201).json(req.body);
 });
+
 // é necessário definir as rotas da API utilizando o método apropriado, (no exemplo abaixo estamos definindo uma rota GET para /users com método HTTP)
 app.get('/users', (req, res) =>{
-    res.send('Ok, deu bom')
+    res.status(200).json(users)
 });
+
 // é necessário iniciar o servidor na porta desejada
 app.listen(3000)
